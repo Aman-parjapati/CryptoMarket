c819f0c83f8a892729c48b608ba01d0cbbb75774
Aman-parjapati <a63092253@gmail.com>
2025-07-20 00:34:03 +0530

    Homepage

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className="hero">
+        <h1>Largest <br/>Crypto Marketplace</h1>
+        <p>Welcome to the world's largest cryptocurrency marketplace. Signup to explore more about cryptos.</p>
+        <form>
+          <input type='text' placeholder='Search cryptos...'/>
+          <button type='submit'>Search</button>
+        </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{textAlign:"center"}}>24hrs change</p>
+          <p className='market-cap'>Market cap</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
